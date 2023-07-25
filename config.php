9739ff11ab5b057260d6c3deac2e8bc022b46004
HugoC9 <105570327+Xassus@users.noreply.github.com>
2023-07-25 09:50:16 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 return array(
 
 	'config' => array(
-		'base_url' => 'http://http://room-booking/',
+		'base_url' => 'https://example.com/classroombookings/',
 		'log_threshold' => 1,
 		'index_page' => 'index.php',
 		'uri_protocol' => 'REQUEST_URI',
@@ -13,9 +13,9 @@ return array(
 	'database' => array(
 		'dsn' => '',
 		'hostname' => '127.0.0.1',
-		'username' => 'setup',
-		'password' => 'Swinglow2021',
-		'database' => 'crbs_db',
+		'username' => 'db_user',
+		'password' => 'db_pass',
+		'database' => 'db_name',
 		'dbdriver' => 'pdo',
 		'subdriver' => 'mysql',
 	),
