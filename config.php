ff104c0788391145795eeada53df93b0e0b55c08
HugoC9 <105570327+Xassus@users.noreply.github.com>
2023-07-25 09:40:47 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+return array(
+
+	'config' => array(
+		'base_url' => 'http://room-booking/',
+		'log_threshold' => 1,
+		'index_page' => 'index.php',
+		'uri_protocol' => 'REQUEST_URI',
+	),
+
+	'database' => array(
+		'dsn' => '',
+		'hostname' => '127.0.0.1',
+		'username' => 'db_user',
+		'password' => 'db_pass',
+		'database' => 'db_name',
+		'dbdriver' => 'pdo',
+		'subdriver' => 'mysql',
+	),
+
+);
