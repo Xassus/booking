4ca01b72f8a3aacd868eaa8562d62c1e94ca6f80
HugoC9 <105570327+Xassus@users.noreply.github.com>
2023-07-25 11:30:09 +0100

    Update config.php

--- config.php
+++ config.php
@@ -13,9 +13,9 @@ return array(
 	'database' => array(
 		'dsn' => '',
 		'hostname' => '127.0.0.1',
-		'username' => 'setup',
-		'password' => 'Swinglow2021',
-		'database' => 'crbs_db',
+		'username' => 'db_user',
+		'password' => 'db_pass',
+		'database' => 'db_name',
 		'dbdriver' => 'pdo',
 		'subdriver' => 'mysql',
 	),
